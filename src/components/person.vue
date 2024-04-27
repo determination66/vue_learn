623559cf4f07b85906dd1b5d75a8da1f4603114a
dongchuliang <2496417370@qq.com>
2024-04-27 10:21:53 +0800

    vue2兼容

---
+++ src/components/person.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="person">
+    <h2>姓名：{{name}}</h2>
+    <h2>年龄：{{age}}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">年龄+1</button>
+    <button @click="showTel">点我查看联系方式</button>
+
+
+</div>
+
+
+</template>
+
+
+<script lang="ts">
+export default {
+    name: "App",
+    data() {
+        return {
+            name: "张三",
+            age: 18,
+            tel: "18552222222",
+        }
+    },
+    methods: {
+        changeName(){
+            this.name="李四"
+        },
+        changeAge(){
+            this.age=20
+        },
+        showTel(){
+            alert(this.tel)
+        }
+    }
+
+
+
+
+}
+
+
+
+
+</script>
+
+
+<style></style>
